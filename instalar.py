6289539e843a933420f296834e139c02c994e29e
Victor Fresno Gomez <63149516+vfg27@users.noreply.github.com>
2022-01-17 20:30:20 +0100

    Update instalar.py

--- instalar.py
+++ instalar.py
@@ -6,7 +6,7 @@ from subprocess import call
 call(["echo 'y' | sudo apt-get update"], shell=True)
 call(["sudo apt-get install \ ca-certificates \ curl \ gnupg \ lsb-release"], shell=True)
 call(["curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"], shell=True)
-call(["echo \ 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"], shell=True)
+call(["echo \ "+r'"'+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable"+r'"'+" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"], shell=True)
 call(["echo 'y' | sudo apt-get update"], shell=True)
 call(["sudo apt-get install docker-ce docker-ce-cli containerd.io"], shell=True)
 call(["sudo docker run hello-world"], shell=True)
